4f479809fbf129b4072884b895dcca0c99a5f213
Patcha96 <eryk.grzeskow@gmail.com>
2026-07-11 18:18:26 +0800

    feat: db connection pool utilization, user insertion logic, db query success testing

---
+++ src/routes/subscriptions.rs
@@ -0,0 +1,38 @@
+use actix_web::{HttpResponse, web};
+use chrono::Utc;
+use sqlx::PgPool;
+use uuid::Uuid;
+
+#[derive(serde::Deserialize)]
+pub struct FormData {
+    email: String,
+    name: String,
+}
+
+pub async fn subscribe(
+    form: web::Form<FormData>,
+    // connection comes from app state (.app_data)
+    db_pool: web::Data<PgPool>,
+) -> HttpResponse {
+    match sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    // get_ref gives us an immutable reference to `PgConnection`
+    // wrapped by `web::Data`.
+    .execute(db_pool.get_ref())
+    .await
+    {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(e) => {
+            println!("Failed to execute query: {}", e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
+}
---
+++ src/startup.rs
@@ -0,0 +1,22 @@
+use crate::routes::{health_check, subscribe};
+use actix_web::dev::Server;
+use actix_web::{App, HttpServer, web};
+use sqlx::PgPool;
+use std::net::TcpListener;
+
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    // wrap connection in a smart pointer & shadow variable
+    let db_pool = web::Data::new(db_pool);
+    // capture `connection` from the surrounding environment
+    let server = HttpServer::new(move || {
+        App::new()
+            .route("health_check", web::head().to(health_check))
+            .route("subscriptions", web::post().to(subscribe))
+            // register connection as part of app state (pointer copy)
+            .app_data(db_pool.clone())
+    })
+    .listen(listener)?
+    .run();
+
+    Ok(server)
+}
