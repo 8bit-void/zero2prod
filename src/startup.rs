700bac2a5c0f4a37c6b8c54f040c128aad0e8eaa
Patcha96 <eryk.grzeskow@gmail.com>
2026-07-19 13:04:58 +0800

    changed health check endpoint from head to get to match digital ocean

--- src/startup.rs
+++ src/startup.rs
@@ -13,7 +13,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
         App::new()
             // middleware added using `wrap` method on `App`
             .wrap(TracingLogger::default())
-            .route("health_check", web::head().to(health_check))
+            .route("health_check", web::get().to(health_check))
             .route("subscriptions", web::post().to(subscribe))
             // register connection as part of app state (pointer copy)
             .app_data(db_pool.clone())
